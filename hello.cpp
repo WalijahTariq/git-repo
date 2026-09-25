2239833ca3de486c5b019175f4c89b694fcad0f4
Walijah Tariq <232003@students.au.edu.pk>
2026-09-25 14:58:18 +0500

    Add Git Bash greeting

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello GitHub!" << endl;
     cout << "This line was added on GitHub!" << endl;
+    cout << "Hello from Git Bash!" << endl;
     return 0;
 }
